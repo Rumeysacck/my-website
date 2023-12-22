98cd160505a7ac8fe31b46ff4a4c96feed125798
Rumeysacck <rumeysa7cicek@gmail.com>
2023-12-22 20:52:30 +0300

    photos changes

--- MyWebsite/wwwroot/js/site.js
+++ MyWebsite/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#altIcerik').addClass('gorunur');
+        } else {
+            $('#altIcerik').removeClass('gorunur');
+        }
+    });
+});
