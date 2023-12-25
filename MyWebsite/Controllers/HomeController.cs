4fcb31f1aaf45f78ea552335e8c2fa706546b861
Rumeysacck <rumeysa7cicek@gmail.com>
2023-12-25 21:25:47 +0300

    The design of the home page is finished

--- MyWebsite/Controllers/HomeController.cs
+++ MyWebsite/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 using Microsoft.AspNetCore.Mvc;
 using MyWebsite.Models;
 
@@ -50,5 +52,46 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+    [HttpPost]
+    public IActionResult Contact(string name, string surname, string subject, string email, string message)
+    {
+        // Burada e-posta gönderme işlemini gerçekleştir
+        try
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("gonderen_email@gmail.com");
+            mail.To.Add("alici_email@gmail.com");
+            mail.Subject = subject;
+            mail.Body = $"Name: {name} {surname}\nEmail: {email}\n\n{message}";
+
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("gonderen_email@gmail.com", "gonderen_email_sifresi");
+            smtpServer.EnableSsl = true;
+
+            smtpServer.Send(mail);
+
+            ViewBag.MessageSent = "Your message has been sent successfully.";
+        }
+        catch (Exception ex)
+        {
+            ViewBag.MessageSent = "Error sending message: " + ex.Message;
+        }
+
+        return View("Contact");
+    }
+
+
+
+
+
+
+
+
+
+
 }
 
